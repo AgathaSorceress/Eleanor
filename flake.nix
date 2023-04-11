24b3ad6e1926c85238bfa546cf51c65d09923ad6
Agatha V. Lovelace <agatha@technogothic.net>
2023-04-11 21:53:52 +0200

    Package for Nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in {
+        packages.default = naersk-lib.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [ openssl ];
+        };
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+              rust-analyzer
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
+}
