3487e80c98f2b065eb892d85ef5e8c920bb1698a
Agatha V. Lovelace <agatha@technogothic.net>
2024-01-27 20:40:37 +0100

    Add CI and Nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+
+        nativeBuildInputs = with pkgs; [ pkg-config alsaLib ];
+        buildInputs = with pkgs; [ openssl ];
+      in {
+        packages.default = naersk-lib.buildPackage {
+          src = ./.;
+          inherit nativeBuildInputs buildInputs;
+        };
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+              rust-analyzer
+            ] ++ buildInputs;
+            inherit nativeBuildInputs;
+
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
+}
